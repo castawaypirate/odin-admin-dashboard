24b3ffdd9afd2822b0b3cb54048fd2ce9b8fa3b3
castawaypirate <apollonas@csd.auth.gr>
2026-03-18 10:26:21 +0200

    Final design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("test");
+let bell = document.querySelector("#bell");
+
+bell.addEventListener("click", function () {
+  let img = bell.querySelector("img");
+
+  if (this.classList.contains("ringed")) {
+    this.classList.remove("ringed");
+    img.src = "./assets/bell-outline.svg";
+  } else {
+    this.classList.add("ringed");
+    img.src = "./assets/bell-ring-outline.svg";
+  }
+});
